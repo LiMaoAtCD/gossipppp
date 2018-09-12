99b3a8228349ead71878d184b17313cfe762305e
limao <limitscofield@gmail.com>
2018-09-12 10:25:52 +0800

    static

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-const Koa = require("koa");
-const koaRouter = require('koa-router');
-const koaBodyParser = require('koa-bodyparser');
+const Koa = require("koa")
+const koaRouter = require('koa-router')
+const koaBodyParser = require('koa-bodyparser')
 const config = require('./config/config')
 const mongoose = require('mongoose')
 const userRouter = require('./routes/user')
 const managerRouter = require('./routes/manager')
+const static = require('koa-static')
 
 mongoose.Promise = global.Promise
 
-const baseRouter = new koaRouter();
+const baseRouter = new koaRouter()
 const app = new Koa();
+const staticPath = './static'
 
 
-app.use(koaBodyParser());
+baseRouter.prefix("/v1/")
+baseRouter.use(userRouter.routes())
+baseRouter.use(managerRouter.routes())
 
-baseRouter.prefix("/v1/");
-baseRouter.use(userRouter.routes());
-baseRouter.use(managerRouter.routes());
-
-app.use(baseRouter.routes());
-app.use(baseRouter.allowedMethods());
+app.use(koaBodyParser())
+    .use(baseRouter.routes())
+    .use(baseRouter.allowedMethods())
+    .use(static(path.join(__dirname, staticPath)))
 
 
 
